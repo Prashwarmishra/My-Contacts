7f4ebd6ec66c51efb861a09e42ac002330f8d0ab
prashwar <prashwarmishra@gmail.com>
2020-05-26 21:05:15 +0530

    1st commit: Create the directory, initialise server, install express and ejs

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const path = require("path");
+
+const port = 8000;
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, 'views'));
+
+app.use(express.urlencoded());
+
+let contactList = [
+    {
+        name: 'Prashwar',
+        phone: 7406610689,
+    },
+    {
+        name: 'Pratyush',
+        phone: 7259389506,
+    },
+    {
+        name: 'Risabh',
+        phone: 7899774171
+    }
+]
+
+app.get('/', function(req, res){
+    res.render("home", {
+        title: "My Contacts",
+        contact_list: contactList,
+    })
+})
+
+app.listen(port, function(err){
+    if (err){
+        console.log(`There was an error in running the server: ${err}`);
+        return
+    }
+    console.log(`The server is up and running at the port: ${port}`);
+});
